8fed4df8b4f05b4ce66e10cfb2b3b11463f90599
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2021-06-22 07:39:37 -0700

    created backend git

---
+++ server.js
@@ -0,0 +1,48 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let cors = require('cors');
+let bodyParser = require('body-parser');
+//let dbConfig = require('./database/db');
+const createError = require('http-errors');
+
+// Express Route
+const jobRoute = require('../backend/routes/job.routes')
+
+// Connecting mongoDB Database
+mongoose.Promise = global.Promise;
+const uri = 'mongodb+srv://TrackerAdmin:TrackerAdminPassword@cluster0.euzmb.mongodb.net/TrackerDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB Connected…')
+})
+.catch(err => console.log(err))
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+app.use('/jobs', jobRoute)
+
+
+// PORT
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+// 404 Error
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
